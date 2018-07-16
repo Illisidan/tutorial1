f5ece0b4132fef1e4b8e96b7978c9a09502543ba
Illisidan <gi.jon.531@gmail.com>
2018-07-17 00:59:33 +0800

    Test2

--- Array/src/com/company/Main.java
+++ Array/src/com/company/Main.java
@@ -5,5 +5,30 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int a, b;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter amount of integers in your array: ");
+        a = input.nextInt();
+        int c[] = new int[a];
+        System.out.println("Input numbers to be sorted: ");
+        for (int i = 0; i < a; i++) {
+            c[i] = input.nextInt();
+        }
+        for (int i = 0; i < a; i++) {
+            for (int j = i + 1; j < a; j++) {
+                if (c[i] < c[j]) {
+                    b = c[i];
+                    c[i] = c[j];
+                    a = b;
+                }
+            }
+        }
+        for (int i = 0; i < a - 1; i++ )
+        {
+            System.out.println(c[a-1]);
+        }
     }
-}
+
+
+
+    }
